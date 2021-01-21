1a72179a4209dbf29566feb66e516a2eafc8d49c
zkostop <zoe.kostopoulos@hva.nl>
2021-01-21 01:49:01 +0100

    update

---
+++ scripts/like.js
@@ -0,0 +1,14 @@
+console.log("loaded.");
+
+const items = document.querySelectorAll(".heart");
+
+items.forEach(function (item, index) {
+	item.addEventListener("click", function () {
+		item.classList.toggle('liked');
+		if (item.classList.contains('liked')) {
+			item.src = "https://upload.wikimedia.org/wikipedia/commons/1/10/Eo_circle_red_heart.svg";
+		} else {
+			item.src = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Eo_circle_teal_white_heart.svg";
+		}
+	})
+});
